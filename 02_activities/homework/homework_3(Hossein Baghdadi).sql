86bf9f73ce3fcba8911d42e6a556adbd8d2092c8
Hossien Baghdadi <you@example.com>
2024-09-21 05:11:42 -0500

    Uploading the sql file of homewok_3

---
+++ 02_activities/homework/homework_3(Hossein Baghdadi).sql
@@ -0,0 +1,98 @@
+-- AGGREGATE
+/* 1. Write a query that determines how many times each vendor has rented a booth 
+at the farmer’s market by counting the vendor booth assignments per vendor_id. */
+
+SELECT 
+vbs.booth_number
+,booth_description
+,booth_price_level
+,market_date
+,COUNT(vendor_id) AS booth_rental
+
+FROM vendor_booth_assignments AS vbs
+INNER JOIN booth as b
+	ON vbs.booth_number = B.booth_number
+GROUP BY vendor_id;
+
+/* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
+sticker to everyone who has ever spent more than $2000 at the market. Write a query that generates a list 
+of customers for them to give stickers to, sorted by last name, then first name. 
+
+HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
+
+SELECT 
+c.customer_id
+,customer_last_name
+,customer_first_name
+,transaction_time
+,SUM(quantity*cost_to_customer_per_qty)AS total_spent
+
+FROM customer AS c
+INNER JOIN customer_purchases AS cp
+ON c.customer_id = cp.customer_id
+
+GROUP BY 
+customer_last_name
+,customer_first_name
+
+HAVING total_spent>2000;
+
+--Temp Table
+/* 1. Insert the original vendor table into a temp.new_vendor and then add a 10th vendor: 
+Thomass Superfood Store, a Fresh Focused store, owned by Thomas Rosenthal
+
+HINT: This is two total queries -- first create the table from the original, then insert the new 10th vendor. 
+When inserting the new vendor, you need to appropriately align the columns to be inserted 
+(there are five columns to be inserted, I've given you the details, but not the syntax)
+
+-> To insert the new row use VALUES, specifying the value you want for each column:
+VALUES(col1,col2,col3,col4,col5) 
+*/
+DROP TABLE IF EXISTS temp.new_vendor;
+
+CREATE TEMP TABLE temp.new_vendor AS
+	SELECT *
+	FROM vendor;
+	
+INSERT INTO temp.new_vendor
+VALUES
+(
+10
+,'Thomass Superfood Store'
+,'a Fresh Focused store'
+,'Thomas'
+,'Rosenthal'
+);
+
+SELECT *
+FROM temp.new_vendor;
+
+-- Date
+/*1. Get the customer_id, month, and year (in separate columns) of every purchase in the customer_purchases table.
+
+HINT: you might need to search for strfrtime modifers sqlite on the web to know what the modifers for month 
+and year are! */
+
+SELECT 
+customer_id
+,STRFTIME('%m',market_date)AS month
+,STRFTIME('%Y',market_date)AS year
+,product_id
+FROM customer_purchases;
+
+/* 2. Using the previous query as a base, determine how much money each customer spent in April 2022. 
+Remember that money spent is quantity*cost_to_customer_per_qty. 
+
+HINTS: you will need to AGGREGATE, GROUP BY, and filter...
+but remember, STRFTIME returns a STRING for your WHERE statement!! */
+
+SELECT 
+customer_id
+,STRFTIME('%m',market_date)AS month
+,STRFTIME('%Y',market_date)AS year
+,product_id
+,SUM(quantity*cost_to_customer_per_qty)AS total_money
+
+FROM customer_purchases
+WHERE month='04' AND year='2022'
+GROUP BY customer_id;
